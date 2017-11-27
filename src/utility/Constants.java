24084ca73bf72ea8df11c6c5a24e78b43642f2c6
Justine Andrew Guiao <justine.guiao@orangeandbronze.com>
2017-11-27 20:15:17 +0800

    Added Z coefficient

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -1,6 +1,10 @@
 package utility;
 
 public class Constants {
+	protected final static int ULTIMATE_OPTIMIZER = 1;
+	protected final static int TRANSPORT_OPTIMIZER = 2;
+	protected final static int EXIT = 0;
+	
 	protected final int CAPACITY = 1;
 	protected final int DEMAND = 0;
 	protected final int Z_COLUMN = 1;
@@ -8,4 +12,7 @@ public class Constants {
 	protected final int OBJECTIVE_FUNCTION_ROW = 1;
 	protected final int SLACK_VARIABLE_COEFFICIENT = 1;
 	protected final String Z_VARIABLE = "= Z";
+	protected final static int Z_COEFFICIENT = 1;
+	
+	
 }
